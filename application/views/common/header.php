9a610750ccb17c018b53ffc0118e6f7fbf5b7fa8
christianlimanto95 <christianlimanto95@gmail.com>
2017-10-13 11:03:56 +0700

    some changes

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,16 +1,22 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
-	
+	<style>
+		@font-face {
+			font-family: roboto-regular;
+			src: url(<?php echo base_url("assets/fonts/Roboto-Regular.ttf"); ?>);
+		}
+	</style>
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/common/default.css"); ?>" />
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/common/header.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/" . $page_name . ".css"); ?>" />
-	
-	<script src="<?php echo base_url("assets/js/common/jquery-3.2.1.min.js"); ?>" defer></script>
-	
 	<?php echo $additional_files; ?>
 </head>
 <body>
 <div class="header">
+	<div class="header-menus">
+		<div class="header-menu">Home</div>
+		<div class="header-menu">Products</div>
+		<div class="header-menu">About</div>
+	</div>
 </div>
-<div class="container">
+<div class="header-space"></div>
--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,42 @@
-<div class="content">
-	<button class="btn-default">Default Button</button>
-	<button class="btn-negative">Negative Button</button>
-	<button class="btn-positive">Positive Button</button><br><br><br>
-	<input type="text" placeholder="this is placeholder" data-type="number" data-thousand-separator="true" />
+<div class="section section-1" style="background-image: url(assets/images/section-1.jpg);">
+	<div class="image-darker"></div>
+	<div class="section-1-center">
+		<div class="section-1-title">Abon Cap Jempol</div>
+		<div class="section-1-description">Kami menyediakan berbagai jenis abon untuk kebutuhan anda</div>
+	</div>
+</div>
+<div class="section section-2">
+	<div class="section-title">Our Products</div>
+	<div class="products-container">
+		<div class="product-item">
+			<div class="product-image" style="background-image: url(assets/images/product-1.jpg);"></div>
+			<div class="product-title">Abon Sapi</div>
+			<div class="product-price">Rp 30.000</div>
+		</div>
+		<div class="product-item">
+			<div class="product-image" style="background-image: url(assets/images/product-1.jpg);"></div>
+			<div class="product-title">Abon Sapi</div>
+			<div class="product-price">Rp 30.000</div>
+		</div>
+		<div class="product-item">
+			<div class="product-image" style="background-image: url(assets/images/product-1.jpg);"></div>
+			<div class="product-title">Abon Sapi</div>
+			<div class="product-price">Rp 30.000</div>
+		</div>
+		<div class="product-item">
+			<div class="product-image" style="background-image: url(assets/images/product-1.jpg);"></div>
+			<div class="product-title">Abon Sapi</div>
+			<div class="product-price">Rp 30.000</div>
+		</div>
+		<div class="product-item">
+			<div class="product-image" style="background-image: url(assets/images/product-1.jpg);"></div>
+			<div class="product-title">Abon Sapi</div>
+			<div class="product-price">Rp 30.000</div>
+		</div>
+		<div class="product-item">
+			<div class="product-image" style="background-image: url(assets/images/product-1.jpg);"></div>
+			<div class="product-title">Abon Sapi</div>
+			<div class="product-price">Rp 30.000</div>
+		</div>
+	</div>
 </div>
